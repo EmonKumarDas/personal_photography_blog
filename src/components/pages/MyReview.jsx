04b0770073a20c149ba369a763b077de18a214ea
emon <emondas457@gmail.com>
2022-11-09 20:07:57 +0600

    add my review page

--- src/components/pages/MyReview.jsx
+++ src/components/pages/MyReview.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { userContext } from '../context/ContextProvider';
 import ReviewCard from '../smallPages/MyReviewCard/ReviewCard';
 
 const MyReview = () => {
+const {user} = useContext(userContext);
+    const [myComments,setComments] = useState([]);
+console.log(myComments);
+    useEffect(()=>{
+        fetch(`http://localhost:5000/comments?email=${user?.email}`)
+        .then(res=>res.json())
+        .then(result=>setComments(result))
+    },[user?.email])
+
     return (
        <div className='my-10'>
-         <ReviewCard></ReviewCard>
+         {
+            myComments.map(mycomment=><ReviewCard
+            key={mycomment._id}
+            mycomment={mycomment}
+            ></ReviewCard>)
+         }
        </div>
     );
 };
