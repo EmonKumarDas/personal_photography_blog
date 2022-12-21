818ffcec556cec83457fefd78e4920118a1a61f0
emon <emondas457@gmail.com>
2022-12-21 14:52:00 +0600

    loading add

--- src/components/UserCommet/Comment.jsx
+++ src/components/UserCommet/Comment.jsx
@@ -4,36 +4,38 @@ import { userContext } from '../context/ContextProvider';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import useTitle from '../Hook/UseHook';
-const Comment = ({ children,id,postData }) => {
+const Comment = ({ children, id, postData }) => {
     const { user } = useContext(userContext);
+    const [loading, setLoading] = useState(false);
     useTitle("Comment")
     // get post info
-    const { title, img} = postData;
-    
+    const { title, img } = postData;
+
     // get user data
     let email = user?.email;
     let name = user?.displayName;
     let photo = user?.photoURL;
     let dateTime = new Date();
-    var date = dateTime.getFullYear()+'-'+(dateTime.getMonth()+1)+'-'+dateTime.getDate();
+    var date = dateTime.getFullYear() + '-' + (dateTime.getMonth() + 1) + '-' + dateTime.getDate();
     var time = dateTime.getHours() + ":" + dateTime.getMinutes() + ":" + dateTime.getSeconds();
 
     // configure rating
-    const [ratingOne,setRatingOne] = useState(0);
-    const [ratingTwo,setRatingTwo] = useState(1);
-    const [ratingThree,setRatingThree] = useState(2);
-    const[isSubmit,setSubmit] = useState(false);
-    let total_rating = (ratingOne+ratingTwo+ratingThree)-3;
+    const [ratingOne, setRatingOne] = useState(0);
+    const [ratingTwo, setRatingTwo] = useState(1);
+    const [ratingThree, setRatingThree] = useState(2);
+    const [isSubmit, setSubmit] = useState(false);
+    let total_rating = (ratingOne + ratingTwo + ratingThree) - 3;
 
     // send comment to the database
     const naviagate = useNavigate()
     const handleMessage = (e) => {
-        if(!user){
+        setLoading(true)
+        if (!user) {
             e.preventDefault();
             naviagate('/login');
             toast("login before comment");
         }
-        else if(user){
+        else if (user) {
             e.preventDefault();
             const message = e.target.message.value;
             let messages = {
@@ -41,14 +43,14 @@ const Comment = ({ children,id,postData }) => {
                 name,
                 email,
                 photo,
-                rating:total_rating,
+                rating: total_rating,
                 date,
                 time,
                 title,
                 img,
-                cateId:id
+                cateId: id
             }
-    
+
             fetch('https://photograpy-server.vercel.app/comments', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
@@ -59,11 +61,11 @@ const Comment = ({ children,id,postData }) => {
                     e.target.message.value = "";
                     setSubmit(true);
                     window.location.reload(true);
-                    toast("Message send")
-                
+                    toast("Comment added")
+                    setLoading(false)
                 })
         }
-        
+
     }
 
     return (
@@ -77,29 +79,29 @@ const Comment = ({ children,id,postData }) => {
                     <textarea rows="3" required name="message" placeholder="Write Your Message..." className="p-4 rounded-md resize-none dark:text-gray-100 dark:bg-gray-900"></textarea>
                     {/* Ratting  start*/}
                     <p className="dark:text-gray-400">Please Rate My Photography</p>
-            <div className="flex flex-wrap items-center mt-2 mb-1 space-x-2">
-			<div className="flex">
-				<button onClick={()=>setRatingOne(ratingOne+1)} disabled = {ratingOne===1 ? true:false} title="Rate 1 stars" aria-label="Rate 1 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill={ratingOne===1?"currentColor":""} className="w-6 h-6 dark:text-yellow-500">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
-				<button onClick={()=>setRatingTwo(ratingTwo+1)} disabled = {ratingTwo===2 ? true:false} title="Rate 2 stars" aria-label="Rate 2 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill={ratingTwo===2?"currentColor":""} className="w-6 h-6 dark:text-yellow-500">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
-				<button onClick={()=>setRatingThree(ratingThree+1)} disabled = {ratingThree===3 ? true:false} title="Rate 3 stars" aria-label="Rate 3 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill={ratingThree===3?"currentColor":""} className="w-6 h-6 dark:text-yellow-500">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
+                    <div className="flex flex-wrap items-center mt-2 mb-1 space-x-2">
+                        <div className="flex">
+                            <button onClick={() => setRatingOne(ratingOne + 1)} disabled={ratingOne === 1 ? true : false} title="Rate 1 stars" aria-label="Rate 1 stars">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill={ratingOne === 1 ? "currentColor" : ""} className="w-6 h-6 dark:text-yellow-500">
+                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                </svg>
+                            </button>
+                            <button onClick={() => setRatingTwo(ratingTwo + 1)} disabled={ratingTwo === 2 ? true : false} title="Rate 2 stars" aria-label="Rate 2 stars">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill={ratingTwo === 2 ? "currentColor" : ""} className="w-6 h-6 dark:text-yellow-500">
+                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                </svg>
+                            </button>
+                            <button onClick={() => setRatingThree(ratingThree + 1)} disabled={ratingThree === 3 ? true : false} title="Rate 3 stars" aria-label="Rate 3 stars">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill={ratingThree === 3 ? "currentColor" : ""} className="w-6 h-6 dark:text-yellow-500">
+                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                </svg>
+                            </button>
 
-			</div>
-			<span className="dark:text-gray-400">{isSubmit===true?total_rating===0:total_rating} out of 3</span>
-		</div>
-            {/* Ratting  end*/}
-                    <button className="py-4 my-8 font-semibold rounded-md dark:text-gray-900 dark:bg-blue-400">Leave feedback</button>
+                        </div>
+                        <span className="dark:text-gray-400">{isSubmit === true ? total_rating === 0 : total_rating} out of 3</span>
+                    </div>
+                    {/* Ratting  end*/}
+                    <button className="py-4 my-8 font-semibold rounded-md dark:text-gray-900 dark:bg-blue-400">{loading ? "Loading..." : "Leave feedback"}</button>
                 </form>
 
             </div>
